07751269acdabb9dae25bf5a3bb49a60f3082846
danila polevshikov <prizolov@inbox.ru>
2017-07-27 23:58:36 +0300

    Test azure mongodb

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: 'mongodb://localhost:27017/hqTest',
+  url: 'mongodb://kinetic-service-db:oAh7WBdBIlG9eozRmRpOt3X4dsS6dgs40j8k0ZDFJGc7l4Ub8HYwI92CjODXA7bKPVhYIyYKGRvUxdqufJ0hHw==@kinetic-service-db.documents.azure.com:10255/hqTest?ssl=true',
   user: '',
   password: '',
   options: {promiseLibrary: global.Promise}
